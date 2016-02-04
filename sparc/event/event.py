e49cc5b25735a44bac71dfbeb9b031ea7d78a2b8
David Davis <davisd50@gmail.com>
2016-02-04 13:25:26 -0500

    add schema checking for datetime field in event implementation

--- sparc/event/event.py
+++ sparc/event/event.py
@@ -1,5 +1,6 @@
 from zope.component.factory import Factory
 from zope.interface import implements
+from zope.schema.fieldproperty import FieldProperty
 from sparc.entity import SparcEntity
 from interfaces import IEvent
 
@@ -10,6 +11,8 @@ class SparcEvent(SparcEntity):
         super(SparcEvent, self).__init__(**kwargs)
         self._entities = kwargs['entities'] if 'entities' in kwargs else []
         self.datetime = kwargs['datetime']
+
+    datetime = FieldProperty(IEvent['datetime'])
 
     def entities(self):
         for entity in self._entities:
